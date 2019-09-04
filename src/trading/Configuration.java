9c7529973de874bad54f15ec74edb3eb536488ae
unknown <theodkul@gmail.com>
2019-09-04 16:15:52 -0400

    Add config

--- src/trading/Configuration.java
+++ src/trading/Configuration.java
@@ -3,13 +3,12 @@ package trading;
 public class Configuration{
 	private Configuration(){}
 
-	public static final String USERNAME = "***REMOVED***";
-	public static final String PASSWORD = "***REMOVED***";
-	public static final String SHARED_SECRET = "***REMOVED***";
-	public static final String IDENTITY_SECRET = "***REMOVED***";
-	public static final String OWNER_ID = "***REMOVED***"; 
-	public static final String API_KEY = "***REMOVED***";
-	public static final String API_TOKEN = "***REMOVED***";
-	public static final String BOT_ID = "***REMOVED***";
-	public static final String BOT_ID3 = "***REMOVED***";
+	public static final String USERNAME = "";
+	public static final String PASSWORD = "";
+	public static final String SHARED_SECRET = "";
+	public static final String IDENTITY_SECRET = "";
+	public static final String OWNER_ID = ""; 
+	public static final String API_KEY = "";
+  	public static final String BOT_ID = "";
+	public static final String BOT_ID3 = "";
 }
