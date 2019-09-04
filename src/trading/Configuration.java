cc7a7529646a5a27fd1d3ffede172c6ca23d7c53
unknown <theodkul@gmail.com>
2019-09-04 16:15:52 -0400

    Add config

--- src/trading/Configuration.java
+++ src/trading/Configuration.java
@@ -3,13 +3,12 @@ package trading;
 public class Configuration{
 	private Configuration(){}
 
-	public static final String USERNAME = "JdoxEdgelordBot";
-	public static final String PASSWORD = "InitialCapitol100";
-	public static final String SHARED_SECRET = "rMUjmEw4Dsy09VcDGH5gSlH082w=";
-	public static final String IDENTITY_SECRET = "P0V5FpHbXbNq/2O4kXR77qE4je4=";
-	public static final String OWNER_ID = "76561198064569486"; 
-	public static final String API_KEY = "5a67925244325a38ee1ed979";
-	public static final String API_TOKEN = "5a1ed656cf6c755f2034dea6";
-	public static final String BOT_ID = "76561198448207173";
-	public static final String BOT_ID3 = "[U:1:487941445]";
+	public static final String USERNAME = "";
+	public static final String PASSWORD = "";
+	public static final String SHARED_SECRET = "";
+	public static final String IDENTITY_SECRET = "";
+	public static final String OWNER_ID = ""; 
+	public static final String API_KEY = "";
+  	public static final String BOT_ID = "";
+	public static final String BOT_ID3 = "";
 }
